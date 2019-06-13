b9a0b3f712b17fd123ea66823ba4dcf7e4ad9ce6
MJ0702 <781082627@qq.com>
2019-06-13 22:37:25 +0800

    add weekend

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+  <div class="title">周末去哪儿</div>
+  <ul>
+    <li class="item border-bottom" v-for="item of weekendList" :key="item.id">
+      <div class="item-img-wrapper">
+        <img class="item-img" :src="item.imgUrl" alt="">
+      </div>
+      <div class="item-info">
+        <p class="item-title">{{item.title}}</p>
+        <p class="item-desc">{{item.desc}}</p>
+      </div>
+    </li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      weekendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/15/66f14e0fd6fbb.jpg_r_640x214_5d69f21d.jpg',
+        title: '锦绣中华民俗村',
+        desc: '一步迈进历史，一天游遍中华'
+      },
+      {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/b8/5d599bbdcf8b57.jpg_r_640x214_2ee055e3.jpg',
+        title: '世界之窗',
+        desc: '国际啤酒节清凉一夏，夜场49.9元'
+      },
+      {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/fa/ca65fde9677de2.jpg_r_640x214_4500e3ff.jpg',
+        title: '海洋奇梦馆',
+        desc: '海洋奇梦馆，带你寻找童年的海洋梦'
+      },
+      {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1602/88/bf120edeaea383.jpg_r_640x214_f8591f7b.jpg',
+        title: 'Skyland未来科技馆',
+        desc: '深圳新的亲子游乐打卡圣地'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+   @import '~styles/mixins.styl'
+ .title
+    margin-top:.2rem
+    line-height:.8rem
+    background:#eee
+    text-indent:.2rem
+  .item-img-wrapper
+    overflow:hidden
+    height:0
+    padding-bottom:33.9%
+    .item-img
+      width:100%
+  .item-info
+    padding:.1rem
+    .item-title
+      line-height:.54rem
+      font-size:.32rem
+      ellipsis()
+    .item-desc
+      line-height:.4rem
+      color:#ccc
+      ellipsis()
+</style>
